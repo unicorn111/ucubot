ffa54aa1bc74598ffaf882f68bda76685a02dd74
Marta Pryshlyak <pryshlyak@ucu.edu.ua>
2018-06-27 10:34:01 +0300

    Update lesson_signal2.sql

--- ucubot/Scripts/lesson_signal2.sql
+++ ucubot/Scripts/lesson_signal2.sql
@@ -6,7 +6,7 @@ CREATE TABLE lesson_signal (
      signal_type SMALLINT,
      student_id INT NOT NULL,
      PRIMARY KEY (id, student_id)
-     INDEX Fk_lesson_signal_student_idx (student_id ASC),
+     INDEX FK_lesson_signal_student_idx (student_id ASC),
      CONSTRAINT FK_lesson_signal_student
      FOREIGN KEY (student_id) REFERENCES ucubot.student (id)
      ON DELETE RESTRICT
